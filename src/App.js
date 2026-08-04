6c569c9b102099457d9fc11a10d2307719d21fe0
Isma <nrainlyaa@gmail.com>
2026-08-04 11:54:31 +0800

    Ubah text ke V2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World! Saya dah pandai deploy ke AWS Amplify!</h1>
+        <h1>HELLO FROM V2</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
